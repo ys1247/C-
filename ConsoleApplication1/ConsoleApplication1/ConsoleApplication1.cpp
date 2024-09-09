86574f50ee5483c6a838c1d276c0f18ea5dd00e7
ys1247 <162389328+ys1247@users.noreply.github.com>
2024-09-09 14:20:01 +0900

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+typedef int test_t;
+
+typedef int test_t2;
+typedef unsigned uint32_t;
+int main()
+{
+    test_t sum = 0;
+    
+    int aa = 100;
+    int bb = 200;
+    sum = aa + bb;
+    //endl =="\n"
+    std::cout << "typedef result sum = " << sum << std::endl;
+
+    uint32_t sum2 = 0;
+    bb = 1000;
+    sum2 = aa + bb;
+    std::cout << "\ntypedef result sum = " << sum << std::endl;
+
+    test_t2 sum3 = 0;
+    aa = 1000;
+    bb = 2000;
+    sum3 = aa + bb;
+    std::cout << "\ntypedef result sum = " << sum << std::endl;
+}
+
+
